febc50e1222d01d901ad6e86b212a63c07019fdf
bharg <bharg@192.168.0.5>
2020-04-11 16:59:41 +0530

    commit

---
+++ capg-movie-info/src/main/java/com/capg/movieinfo/CapgMovieInfoApplication.java
@@ -0,0 +1,17 @@
+package com.capg.movieinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+@SpringBootApplication
+public class CapgMovieInfoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CapgMovieInfoApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+}
---
+++ capg-movie-info/src/main/java/com/capg/movieinfo/model/MovieCatelog.java
@@ -0,0 +1,25 @@
+package com.capg.movieinfo.model;
+public class MovieCatelog {
+	private long id;
+	private String movieName;
+	public MovieCatelog() {
+		 
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getMovieName() {
+		return movieName;
+	}
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+	public MovieCatelog(Long id, String movieName) {
+		super();
+		this.id = id;
+		this.movieName = movieName;
+	}	
+}
